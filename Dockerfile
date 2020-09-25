e730a2bca92a099dcea28d62cc903b7b3bd45fe8
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-25 10:53:07 +0500

    updated

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM ubuntu
 RUN apt-get update
 RUN apt-get install -y python3-pip
 RUN pip3 install  youtube-dl
-ENTRYPOINT ["youtube-dl", "https://youtu.be/CkKmWhtzYHU"]
+ENTRYPOINT ["youtube-dl"]
+CMD ["https://youtu.be/CkKmWhtzYHU"]
--- download-video-using-docker-image.sh
+++ download-video-using-docker-image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# url=$1
-# if [[ $url == "" ]] ; then
-# 	read -p "please Enter you video url: " url
-# fi
-# name=`/home/amir/.local/bin/youtube-dl --get-filename $url`
-# name=`echo "/media/$name" | sed 's/\ /-/g' | sed 's/-\{1,\}/-/g'`
-# sudo docker run -v `pwd`:/media youtube-dl-image $url -o $name
-sudo docker run -v `pwd`:/media youtube-dl-image -o /media/f
+url=$1
+if [[ $url == "" ]] ; then
+	read -p "please Enter you video url: " url
+fi
+name=`/home/amir/.local/bin/youtube-dl --get-filename $url`
+name=`echo "/media/$name" | sed 's/\ /-/g' | sed 's/-\{1,\}/-/g'`
+sudo docker run -v `pwd`:/media youtube-dl-image $url -o $name
+# sudo docker run -v `pwd`:/media youtube-dl-image -o /media/f

fe9a8466113a2df57a3c92d8207402d6f3c3966b
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-09-25 10:37:15 +0500

    updated

--- download-video-using-docker-image.sh
+++ download-video-using-docker-image.sh
@@ -5,5 +5,6 @@ if [[ $url == "" ]] ; then
 fi
 name=`/home/amir/.local/bin/youtube-dl --get-filename $url`
 name=`echo "/media/$name" | sed 's/\ /-/g' | sed 's/-\{1,\}/-/g'`
-echo $name
-# sudo docker run -v `pwd`:/media youtube-dl-image $url -o $name
+# echo $name
+echo $url
+sudo docker run -v `pwd`:/media youtube-dl-image $url -o $name
